bb45cffec6855816d0e2f452aef45d4decdae5d2
Sean Murphy <sean@iamseanmurphy.com>
2025-08-07 23:17:42 +0000

    6.1.6

--- jquery-archive-list-widget/build/index.asset.php
+++ jquery-archive-list-widget/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => 'df1360056842b534ecc2');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '1f5cb807818dd2e7d569');
